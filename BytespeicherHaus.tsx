9cba26a7e06e278863ffa3de130c0ca0688e4aea
fpf2000 <fpf2000@gmx.de>
2021-12-29 06:34:01 +0100

    Chachaoszone.json#start_erfurt gesetzt ?

--- BytespeicherHaus.tsx
+++ BytespeicherHaus.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="BytespeicherHaus" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <properties>
+  <property name="&#10;tilesetCopyright" value="fpf2000"/>
+ </properties>
  <image source="tilesets/BS_Haus.png" width="320" height="320"/>
 </tileset>
